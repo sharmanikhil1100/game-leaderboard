c0db2f4eacd3ffdf832f5c660136e742f0f1f63d
nikhil sharma <sharmanikhil1100@gmail.com>
2024-06-02 13:49:06 +0530

    removed try catch in LeaderboardRepository functions

--- src/main/java/org/example/repository/LeaderboardRepository.java
+++ src/main/java/org/example/repository/LeaderboardRepository.java
@@ -18,24 +18,16 @@ public class LeaderboardRepository {
     private String leaderboardKey;
 
     public void addEntity(LeaderboardModel inputData) {
-        try {
-            this.redisTemplate.opsForZSet().add("leaderboard", inputData, inputData.getScore());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        this.redisTemplate.opsForZSet().add("leaderboard", inputData, inputData.getScore());
     }
 
     public Set<LeaderboardModel> getAll(Constants order) {
         Set<LeaderboardModel> leaderboardModelSet = null;
-        try {
-            if (order == Constants.RANGE) { // ascending order
-                leaderboardModelSet = this.redisTemplate.opsForZSet().range("leaderboard", 0, -1);
-            }
-            else { // descending order
-                leaderboardModelSet = this.redisTemplate.opsForZSet().reverseRange("leaderboard", 0, -1);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (order == Constants.RANGE) { // ascending order
+            leaderboardModelSet = this.redisTemplate.opsForZSet().range("leaderboard", 0, -1);
+        }
+        else { // descending order
+            leaderboardModelSet = this.redisTemplate.opsForZSet().reverseRange("leaderboard", 0, -1);
         }
 
         return leaderboardModelSet;
@@ -43,39 +35,25 @@ public class LeaderboardRepository {
 
     public Optional<LeaderboardModel> peekSet(Constants order) {
         Optional<LeaderboardModel> element = null;
-        try {
-            if (order == Constants.RANGE) { // get max element
-                element = this.redisTemplate.opsForZSet().reverseRange("leaderboard", 0, 1).stream().findFirst();
-            }
-            else { // get min element
-                element = this.redisTemplate.opsForZSet().range("leaderboard", 0, 1).stream().findFirst();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (order == Constants.RANGE) { // get max element
+            element = this.redisTemplate.opsForZSet().reverseRange("leaderboard", 0, 1).stream().findFirst();
+        }
+        else { // get min element
+            element = this.redisTemplate.opsForZSet().range("leaderboard", 0, 1).stream().findFirst();
         }
 
         return element;
     }
 
     public Long getCount() {
-        Long setCount = null;
-
-        try {
-            setCount = this.redisTemplate.opsForZSet().count("leaderboard", Integer.MIN_VALUE, Integer.MAX_VALUE);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return setCount;
+        return this.redisTemplate.opsForZSet().count("leaderboard", Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
 
     public void popSet() {
-        Optional<LeaderboardModel> element = null;
-        try {
-            element.ifPresent(leaderboardModel -> this.redisTemplate.opsForZSet().remove("leaderboard", element));
-            //TODO: do logging
-        } catch (Exception e) {
-            e.printStackTrace();
+        Optional<LeaderboardModel> element = this.peekSet(Constants.REVERSE_RANGE);
+
+        if (element.isPresent()) {
+            this.redisTemplate.opsForZSet().remove("leaderboard", element);
         }
     }
 }
